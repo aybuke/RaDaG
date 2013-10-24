46eafc0e6104aa661a57155a2ccefa4e62c52417
Aybuke Ozdemir <aybuke@kripton.(none)>
2013-10-24 04:05:22 +0300

    Change extension.

--- rdg.rb
+++ rdg.rb
@@ -1,42 +1,72 @@
 # encoding: utf-8
 
-class RaDaG
+text_file=File.open('data.txt')
+file = text_file.readlines()
+text_file.close
 
-  def getRandomint
-      i = Random.rand(0..999_999)
-    return i
-  end
+i=0
+first_line = file[0]
+array1= Array.new
+first_line.each_line(' '){
+|s|
+array1[i]=s
+i=i+1
+}
 
-  def getRandomChar
-    o =  [('a'..'z'),('A'..'Z')].map{|r| r.to_a}.flatten
-    char  =  (0..1).map{ o[rand(o.length)].chr}.join
-    return char
-  end
+j=0
+second_line = file[1]
+array2= Array.new
+second_line.each_line(' '){
+|s|
+array2[j]=s
+j=j+1
+}
 
-  def getRandomDate
-    day=rand(30)
-    mounth=rand(12)
-    year=rand(1000..2500)
-    date= day.to_s+"-"+mounth.to_s+"-"+year.to_s
-    return date
-  end
+array3=file[2]
 
-  def getRandomMail
-    o =  [('a'..'z')].map{|r| r.to_a}.flatten
-    string1 = (0..8).map{ o[rand(o.length)]  }.join
-    string2 = (0..8).map{ o[rand(o.length)]  }.join
-    mail = string1+"@"+string2+".com"
-    return mail
-  end
-    
+how_many = array3.to_i
+start=1
+a=1
+while start <= how_many
+  print "\n", start,"\n"
+    if array1[1].include? 'integer'
+      while a<=array2[1].to_i
+        int = Random.rand(0..999_999)
+        print int,' '
+        a = a+1
+      end
+    end
+    a=1   
+    if array1[2].include? 'char'
+      while a<=array2[2].to_i
+        o = [('a'..'z','A'..'Z')].map{|r| r.to_a}.flatten
+        char = (0..1).map{ o[rand(o.length)].chr}.join
+        print char,' '
+        a=a+1
+      end
+    end
+    a=1
+    if array1[3].include? 'date'
+      while a<=array2[3].to_i
+        day=rand(30)
+        mounth=rand(12)
+        year=rand(1000..2500)
+        date= day.to_s+"-"+mounth.to_s+"-"+year.to_s
+        print date,' '
+        a=a+1
+      end
+    end
+    a=1
+    if array1[4].include? 'email'
+      while a<=array2[4].to_i
+        o = [('a'..'z')].map{|r| r.to_a}.flatten
+        string1 = (0..8).map{ o[rand(o.length)] }.join
+        string2 = (0..8).map{ o[rand(o.length)] }.join
+        mail = string1+"@"+string2+".com"
+        print mail,' '
+        a=a+1
+      end
+    end
+    a=1
+    start=start+1
 end
-
-
-obj4 = RaDaG.new
-puts obj4.getRandomMail()
-obj3 = RaDaG.new
-puts obj3.getRandomDate()
-obj2 = RaDaG.new
-puts obj2.getRandomChar()
-obj = RaDaG.new
-puts obj.getRandomint()
