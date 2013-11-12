297893ea7b220bf173eb2965ed5713dcea541282
aybuke <aybuke.147@gmail.com>
2013-11-12 04:35:14 +0200

    monotype qmail log generated

--- qmail.rb
+++ qmail.rb
@@ -1,9 +1,13 @@
 # encoding: utf-8
 
-class Qmail
+class Parts
 
-  def time_rand
+  def time_rand1
     Time.at(rand * Time.now.to_i).nsec
+  end
+  
+  def time_rand2
+    Time.at(rand * Time.now.to_i)
   end
 
   def ip_rand
@@ -15,7 +19,11 @@ class Qmail
     port = Random.rand(1..1000)
     return port
   end
-
+  
+  def pid_rand
+    pid = Random.rand(1000..65535)
+    return pid
+  end
   def mail_rand
     o =  [('a'..'z')].map{|r| r.to_a}.flatten
     string1 = (0..8).map{ o[rand(o.length)]  }.join
@@ -34,11 +42,30 @@ class Qmail
       return fraction_rand
     end
   end
+  
+  def number_rand
+    k = Random.rand(0..9999)
+    return k
+  end
 end
 
-obj3 = Qmail.new
-puts obj3.time_rand()
-puts obj3.ip_rand()
-puts obj3.mail_rand()
-puts obj3.fraction_rand()
-puts obj3.port_rand()
+class Qmail_Log < Parts
+  def Combine_Parts(number)
+  i=0
+    while i<number
+      obj3 = Parts.new
+      puts "@#{obj3.time_rand1()} rblsmtpd: #{obj3.ip_rand()} pid #{obj3.pid_rand()}: #{obj3.number_rand()} Send mail to honeynet #{obj3.mail_rand()} #{time_rand2()}"
+    i=i+1
+    end
+  end
+end
+
+obj = Qmail_Log.new
+#obj3 = Parts.new
+#puts obj3.time_rand()
+#puts obj3.ip_rand()
+#puts obj3.mail_rand()
+#puts obj3.fraction_rand()
+#puts obj3.port_rand()
+#puts obj3.pid_rand()
+obj.Combine_Parts(2)
