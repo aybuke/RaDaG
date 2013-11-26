48256759f464bb5cab1163ac8cadf56aa81ae6c0
aybuke <aybuke.147@gmail.com>
2013-11-26 05:03:47 +0200

    Added incomplete log function

--- qmail.rb
+++ qmail.rb
@@ -24,12 +24,19 @@ class Parts
     pid = Random.rand(1000..65535)
     return pid
   end
+
   def mail_rand
     o =  [('a'..'z')].map{|r| r.to_a}.flatten
     string1 = (0..8).map{ o[rand(o.length)]  }.join
     string2 = (0..8).map{ o[rand(o.length)]  }.join
     mail = string1+"@"+string2+".com"
     return mail
+  end
+
+  def word_rand
+    o =  [('a'..'z')].map{|r| r.to_a}.flatten
+    word = (0..8).map{ o[rand(o.length)]  }.join
+    return word
   end
 
   def fraction_rand
@@ -50,22 +57,114 @@ class Parts
 end
 
 class Qmail_Log < Parts
-  def Combine_Parts(number)
-  i=0
+
+  def rblsmtpd(number)
+    i=0
     while i<number
-      obj3 = Parts.new
-      puts "@#{obj3.time_rand1()} rblsmtpd: #{obj3.ip_rand()} pid #{obj3.pid_rand()}: #{obj3.number_rand()} Send mail to honeynet #{obj3.mail_rand()} #{time_rand2()}"
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} rblsmtpd: #{obj.ip_rand()} pid #{obj.pid_rand()}: #{obj.number_rand()} Send mail to #{obj.word_rand()} #{obj.mail_rand()} #{time_rand2()}"
     i=i+1
     end
   end
+  
+  def tcpserver1(number)
+    i=0
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} tcpserver: status: #{obj.fraction_rand()}"
+      i=i+1
+    end
+  end
+  
+  def tcpserver2(number)
+    i=0
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} tcpserver: pid #{obj.pid_rand()} from #{obj.ip_rand()}"
+      i=i+1
+    end
+  end
+ 
+  def tcpserver3(number)
+    i=0
+    array = ['ok', 'deny']
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} tcpserver: #{array.sample} #{obj.word_rand()}:#{obj.ip_rand()}:#{obj.port_rand()} #{obj.word_rand()}:#{obj.ip_rand()}:#{obj.port_rand()}"
+      i=i+1
+    end
+  end
+
+  def qmail_send(number)
+    i=0
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} info msg #{obj.number_rand()}: bytes #{obj.number_rand()} from <#{obj.mail_rand()}> qp #{obj.number_rand()} uid #{obj.number_rand()}"
+      i=i+1
+    end
+  end
+  
+  def jgreylist(number)
+    i=0
+    array = ['OK known', 'DENY no reverse DNS', 'GREY first time', 'GREY too soon']
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} jgreylist[#{obj.number_rand()}]: #{obj.ip_rand()} #{array.sample}"
+      i=i+1
+    end
+  end
+  
+  def spf(number)
+    i=0
+    array = ['pass', 'none', 'unknown', 'neutral', 'softfail', 'fail', 'error']
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} qmail-smtpd[[#{obj.number_rand()}]: Received-SPF: #{array.sample} #{obj.word_rand()}:#{obj.ip_rand()}:#{obj.port_rand()}"
+      i=i+1
+    end
+  end
+  
+  def greetdelay(number)
+    i=0
+    array = ['disconnected', 'sent data']
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} qmail-smtpd[[#{obj.number_rand()}]: before greeting: [#{obj.ip_rand()}] client #{array.sample}"
+      i=i+1 
+    end
+  end
+
+  def auth(number)
+    i=0
+    array=['failed','successful']
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} qmail-smtpd[[#{obj.number_rand()}]: AUTH #{array.sample} [#{obj.ip_rand()}] <#{obj.mail_rand()}>"
+      i=i+1
+    end
+  end
+
+  def mailrcpt(number)
+   i=0
+    array=['MAIL FROM', 'RCPT TO']
+    while i<number
+      obj = Parts.new
+      puts "@#{obj.time_rand1()} qmail-smtpd[[#{obj.number_rand()}] #{array.sample} <#{obj.mail_rand()}>"
+      i=i+1
+    end
+  end 
 end
 
+"
 obj = Qmail_Log.new
-#obj3 = Parts.new
-#puts obj3.time_rand()
-#puts obj3.ip_rand()
-#puts obj3.mail_rand()
-#puts obj3.fraction_rand()
-#puts obj3.port_rand()
-#puts obj3.pid_rand()
-obj.Combine_Parts(2)
+obj.rblsmtpd(2)
+obj.tcpserver1(2)
+obj.qmail_send(2)
+obj.tcpserver2(2)
+obj.tcpserver3(3)
+obj.jgreylist(1)
+obj.spf(4)
+obj.greetdelay(1)
+obj.auth(2)
+obj.mailrcpt(1)
+"
