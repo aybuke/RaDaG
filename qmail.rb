45dffd442d6de61e48aa0d1d69a825081a95c51d
aybuke <aybuke.147@gmail.com>
2013-11-28 04:31:29 +0200

    Added comment line.

--- qmail.rb
+++ qmail.rb
@@ -1,30 +1,35 @@
 # encoding: utf-8
 
 class Parts
-
+# generated random time.format: '973492011' 
   def time_rand1
     Time.at(rand * Time.now.to_i).nsec
   end
-  
+
+#generated random time.format: '1972-09-01 03:22:52 +0300'
   def time_rand2
     Time.at(rand * Time.now.to_i)
   end
 
+#generated random ip.format:'115.232.14.162'
   def ip_rand
     ip = "%d.%d.%d.%d" % [rand(256), rand(256), rand(256), rand(256)]
     return ip
   end
-  
+
+#generated random port number.format: '50'
   def port_rand
     port = Random.rand(1..1000)
     return port
   end
-  
+
+#generated random proccess id.format: '28065'
   def pid_rand
     pid = Random.rand(1000..65535)
     return pid
   end
 
+#generated random e-mail.format: 'jyevbjseh@lkqpmckry.com'
   def mail_rand
     o =  [('a'..'z')].map{|r| r.to_a}.flatten
     string1 = (0..8).map{ o[rand(o.length)]  }.join
@@ -33,12 +38,14 @@ class Parts
     return mail
   end
 
+#generated random word.format: 'yszmokxzc'
   def word_rand
     o =  [('a'..'z')].map{|r| r.to_a}.flatten
     word = (0..8).map{ o[rand(o.length)]  }.join
     return word
   end
-
+  
+#generated random fraction number.format: '36/51' 
   def fraction_rand
     i = Random.rand(0..100)
     j = Random.rand(0..100)
@@ -49,7 +56,8 @@ class Parts
       return fraction_rand
     end
   end
-  
+
+#generated random number. format: '7368'
   def number_rand
     k = Random.rand(0..9999)
     return k
@@ -57,7 +65,7 @@ class Parts
 end
 
 class Qmail_Log < Parts
-
+#format: 'TIMESTAMP rblsmtpd: 1.2.3.4 pid 27730: 451 message'
   def rblsmtpd(number)
     i=0
     while i<number
@@ -66,7 +74,8 @@ class Qmail_Log < Parts
     i=i+1
     end
   end
-  
+
+#format: 'TIMESTAMP tcpserver: status 3/50'  
   def tcpserver1(number)
     i=0
     while i<number
@@ -76,6 +85,7 @@ class Qmail_Log < Parts
     end
   end
   
+#format: 'TIMESTAMP tcpserver: pid 27730 from 1.2.3.4'
   def tcpserver2(number)
     i=0
     while i<number
@@ -85,6 +95,7 @@ class Qmail_Log < Parts
     end
   end
  
+#format: 'TIMESTAMP tcpserver: ok 27730 myhostname:myip:myport hostname:ip:ident:port'
   def tcpserver3(number)
     i=0
     array = ['ok', 'deny']
@@ -95,6 +106,7 @@ class Qmail_Log < Parts
     end
   end
 
+#format: 'TIMESTAMP info msg 12345: bytes 3189 from <sender@domain.xyz> qp 27763 uid 46'
   def qmail_send(number)
     i=0
     while i<number
@@ -104,6 +116,7 @@ class Qmail_Log < Parts
     end
   end
   
+#format: 'TIMESTAMP jgreylist[27730]: 1.2.3.4 DENY no reverse DNS'
   def jgreylist(number)
     i=0
     array = ['OK known', 'DENY no reverse DNS', 'GREY first time', 'GREY too soon']
@@ -114,6 +127,7 @@ class Qmail_Log < Parts
     end
   end
   
+#format: 'TIMESTAMP qmail-smtpd[12345]: Received-SPF: unknown (myhostname: ...)'
   def spf(number)
     i=0
     array = ['pass', 'none', 'unknown', 'neutral', 'softfail', 'fail', 'error']
@@ -124,6 +138,7 @@ class Qmail_Log < Parts
     end
   end
   
+#format: 'TIMESTAMP qmail-smtpd[12345]: before greeting: [1.2.3.4] client disconnected'
   def greetdelay(number)
     i=0
     array = ['disconnected', 'sent data']
@@ -134,6 +149,7 @@ class Qmail_Log < Parts
     end
   end
 
+#format: 'TIMESTAMP qmail-smtpd[12345]: AUTH successful [1.2.3.4] user@domain.xyz'
   def auth(number)
     i=0
     array=['failed','successful']
@@ -144,6 +160,7 @@ class Qmail_Log < Parts
     end
   end
 
+#format: 'TIMESTAMP qmail-smtpd[12345]: MAIL FROM:<email@domain.xyz>'
   def mailrcpt(number)
    i=0
     array=['MAIL FROM', 'RCPT TO']
@@ -155,7 +172,7 @@ class Qmail_Log < Parts
   end 
 end
 
-"
+
 obj = Qmail_Log.new
 obj.rblsmtpd(2)
 obj.tcpserver1(2)
@@ -167,4 +184,4 @@ obj.spf(4)
 obj.greetdelay(1)
 obj.auth(2)
 obj.mailrcpt(1)
-"
+
